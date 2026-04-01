dbd30100d09eecc537e5c634b98cd39f69e75eff
moreno_esteban <esteban.roa@correounivalle.edu.co>
2026-04-01 10:38:37 -0500

    CourseServicetest ready

--- cursos/src/test/java/CourseServiceTest.java
+++ cursos/src/test/java/CourseServiceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.plataforma.exception.CourseDuplicateException;
+import com.plataforma.exception.CourseFullException;
 import com.plataforma.model.Course;
+import com.plataforma.model.Student;
 import com.plataforma.service.CourseService;
 
 public class CourseServiceTest {
@@ -15,6 +17,7 @@ public class CourseServiceTest {
     @BeforeEach
     void setUp(){
         courseService = new CourseService();
+        
     }
 
     @Test
@@ -52,6 +55,55 @@ public class CourseServiceTest {
         CourseDuplicateException exception = assertThrows(CourseDuplicateException.class, ()->{
             courseService.addCourseCreated(course2);
         });
+
+    }
+
+    @Test
+    void listAvailableCoursesTest(){
+
+        Course course1 = new Course("1J", "java basic", 1);
+        Course course2 = new Course("1P", "python intermedium", 1);
+        Course course3 = new Course("1S", "spring security", 2);
+
+        Student student = new Student("paquito", 1, "paquito@gmail.com ");
+
+        courseService.addCourseCreated(course1);
+        courseService.addCourseCreated(course2);
+        courseService.addCourseCreated(course3);
+
+        course1.addStudent(student);
+        course2.addStudent(student);
+
+        courseService.listAvailableCourses();
+
+        assertEquals(1, courseService.listAvailableCourses().size());
+
+        
+
+    }
+
+
+    @Test
+    void CoursesFullTest(){
+
+        Course course1 = new Course("1J", "java basic", 1);
+        Course course2 = new Course("1P", "python intermedium", 1);
+        Course course3 = new Course("1S", "spring security", 1);
+
+        Student student = new Student("paquito", 1, "paquito@gmail.com ");
+
+        courseService.addCourseCreated(course1);
+        courseService.addCourseCreated(course2);
+        courseService.addCourseCreated(course3);
+
+        course1.addStudent(student);
+        course2.addStudent(student);
+        course3.addStudent(student);
+
+        CourseFullException exception = assertThrows(CourseFullException.class, ()->{
+            courseService.listAvailableCourses();
+        });
+        
 
     }
 
--- cursos/src/test/java/RegistrationServiceTest.java
+++ cursos/src/test/java/RegistrationServiceTest.java
@@ -3,8 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.plataforma.model.Course;
-import com.plataforma.service.CourseService;
+
 
 public class RegistrationServiceTest {
 
